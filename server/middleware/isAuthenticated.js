d086f223ef35d634fa5731efdf072424ee3b4942
Raushan Kumar <raushankumarguptag@gmail.com>
2024-09-21 21:34:22 +0530

    IsAdmin Middleware added

--- server/middleware/isAuthenticated.js
+++ server/middleware/isAuthenticated.js
@@ -1,27 +1,37 @@
 import TryCatch from "./errorHandler.js";
 import jwt from "jsonwebtoken";
+import { User } from "../models/user.model.js";
 
 export const isAuthenticated = TryCatch(async (req, res, next) => {
   const { authorization } = req.headers;
+
   if (!authorization || !authorization.startsWith("Bearer ")) {
     return res
       .status(401)
-      .json({ message: "No token provided ,User not authenticated" });
+      .json({ message: "No token provided, User not authenticated" });
   }
 
   const token = authorization.split(" ")[1];
-  // verfiy token
 
-  const decoded = jwt.verify(token, process.env.JWT_SECRET);
-  console.log(decoded);
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-  if (!decoded) {
+    req.user = await User.findById(decoded._id);
+
+    next();
+  } catch (err) {
     return res
       .status(401)
-      .json({ message: "User not authenticated, token expired" });
+      .json({ message: "Invalid token, user not authenticated" });
   }
+});
 
-  req.user = decoded.user;
+// for admin
 
+export const isAdmin = TryCatch(async (req, res, next) => {
+  const user = req.user;
+  if (user.role !== "admin") {
+    return res.status(403).json({ message: "Only admin can access this !" });
+  }
   next();
 });
