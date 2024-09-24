46680c9484f54093d8522da8119f6bc3683342ed
Raushan Kumar <raushankumarguptag@gmail.com>
2024-09-24 17:05:17 +0530

    Get Certificate After Course Completion

--- server/controllers/courseProgress.controller.js
+++ server/controllers/courseProgress.controller.js
@@ -1,6 +1,7 @@
 import TryCatch from "../middleware/errorHandler.js";
 import { Lecture } from "../models/lecture.model.js";
 import { Progress } from "../models/progress.model.js";
+import { Course } from "../models/course.model.js";
 
 // add progress
 export const addProgress = TryCatch(async (req, res) => {
@@ -52,4 +53,33 @@ export const getYourProgress = TryCatch(async (req, res) => {
     allLectures,
     progress,
   });
+});
+
+// Download certificate after course completion
+export const downloadCertificate = TryCatch(async (req, res) => {
+  const { id } = req.params;
+
+  const course = await Course.findById(id);
+  if (!course) {
+    return res
+      .status(404)
+      .json({ message: "Course not found", success: false });
+  }
+
+  const progress = await Progress.findOne({
+    user: req.user._id,
+    course: id,
+  });
+
+  if (
+    !progress ||
+    progress.completedLectures.length !==
+      (await Lecture.countDocuments({ course: id }))
+  ) {
+    return res
+      .status(403)
+      .json({ message: "Course not completed", success: false });
+  }
+
+  return res.status(200).json({ message: "Certificate", success: true });
 });
