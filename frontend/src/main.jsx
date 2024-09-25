871522725fb02f4f0285ea1337c7b7c089c25287
Raushan Kumar <raushankumarguptag@gmail.com>
2024-09-25 16:14:47 +0530

    RTK setup and persist the store as well

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { store } from "./redux/store/store.js";
+import { Provider } from "react-redux";
+import persistStore from "redux-persist/es/persistStore";
+import { PersistGate } from "redux-persist/integration/react";
+import { Toaster } from "react-hot-toast";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
+const persistor = persistStore(store);
+
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <RouterProvider router={router} />
+      <Toaster position="top-center" reverseOrder={false} theme="dark" />
+    </PersistGate>
+  </Provider>
+);
---
+++ frontend/src/redux/store/slices/authSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  token: localStorage.getItem("token") || "",
+  isAuthenticated: !!localStorage.getItem("token"),
+  activationToken: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      state.token = action.payload.token;
+      state.isAuthenticated = true;
+      localStorage.setItem("token", action.payload.token);
+    },
+    logout: (state) => {
+      state.token = "";
+      state.isAuthenticated = false;
+      localStorage.removeItem("token");
+    },
+    setActivationToken: (state, action) => {
+      state.activationToken = action.payload;
+    },
+  },
+});
+
+export const { login, logout, setToken } = authSlice.actions;
+export default authSlice.reducer;
